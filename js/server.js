abec102f92f976dcc3daf51c874ae8548888a304
donkimber <donkimber@gmail.com>
2018-04-12 17:18:01 -0700

    made index.html with links and added p2 example that drives servos

--- js/server.js
+++ js/server.js
@@ -1,5 +1,7 @@
 
-var app = require("express")();
+var express = require("express");
+var app = express();
+var bodyParser = require('body-parser');
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
 var five = require("johnny-five");
@@ -7,6 +9,7 @@ var board = new five.Board();
 
 board.on("ready", function() {
   var led = new five.Led(13);
+  var servo = new five.Servo(12);
   led.blink(1000);
 
   io.on("connection", function(socket) {
@@ -15,14 +18,26 @@ board.on("ready", function() {
         console.log("data: "+data);
         led.blink(data);
     });
-    socket.on("servo:value", function(data) {
+    socket.on("servo.set", function(data) {
         console.log("servo value: "+data);
-        //led.blink(data);
+        servo.to(data);
     });
   });
 });
 
+app.use(express.static(__dirname + "/.."));
+app.use(bodyParser.json());
+
 app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.get("/mouseJoint", function (req, res) {
+    var str = __dirname + "/mouseJoint.html"
+    res.sendFile(str);
+});
+
+app.get("/realtime", function (req, res) {
   res.sendFile(__dirname + "/realtime.html");
 });
 
