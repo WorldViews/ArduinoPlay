08b25b3b99963e457a1de5c9ac3d3d4b8892938c
donkimber <donkimber@gmail.com>
2018-04-14 06:27:20 -0700

    playBox gets acc data sent by server

--- js/server.js
+++ js/server.js
@@ -4,22 +4,52 @@ var app = express();
 var bodyParser = require('body-parser');
 var server = require("http").Server(app);
 var io = require("socket.io")(server);
+var Playground = require("playground-io");
 var five = require("johnny-five");
-var board = new five.Board();
+var sock = null;
+//var board = new five.Board();
+var board = new five.Board({
+  io: new Playground({
+    port: "com4",
+
+    // Passing Firmata options through:
+    // Circuit Playground Firmata seems not to report version before timeout,
+    // lower timeout to reduce initial connection time.
+    reportVersionTimeout: 200
+  })
+});
 
 board.on("ready", function() {
   var led = new five.Led(13);
   var servo = new five.Servo(12);
   led.blink(1000);
 
+  var accelerometer = new five.Accelerometer({
+    controller: Playground.Accelerometer
+  });
+
+  accelerometer.on("change", (data) => {
+      //console.log("acc data: "+JSON.stringify(data));
+      console.log("acc data: "+data.x+" "+data.y+" "+data.z);
+      console.log("sock: "+sock);
+      if (sock) {
+          console.log("sending acc.change "+data.x+" "+data.y+" "+data.z);
+          var acc = {x: data.x, y: data.y, z: data.z};
+          //sock.emit("acc.change", data.x+" "+data.y+" "+data.z);
+          sock.emit("acc.change", acc);
+      }
+    //piezo.frequency(data.double ? 1500 : 500, 50);
+  });
+
   io.on("connection", function(socket) {
+      sock = socket;
     console.log("Got connection...");
     socket.on("change:interval", function(data) {
         console.log("data: "+data);
         led.blink(data);
     });
     socket.on("servo.set", function(data) {
-        console.log("servo value: "+data);
+        //console.log("servo value: "+data);
         servo.to(data);
     });
   });
@@ -32,8 +62,8 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.get("/mouseJoint", function (req, res) {
-    var str = __dirname + "/mouseJoint.html"
+app.get("/playBox", function (req, res) {
+    var str = __dirname + "/playBox.html"
     res.sendFile(str);
 });
 
