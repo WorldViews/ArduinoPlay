07f41dfb7c5cc1d52d3864c9778ae09f51d49db7
donkimber <donkimber@gmail.com>
2018-07-11 17:26:09 -0700

    comment out some verbose logging and show updated values in web page

--- Playground/js/server.js
+++ Playground/js/server.js
@@ -8,67 +8,80 @@ var Playground = require("playground-io");
 var five = require("johnny-five");
 var sock = null;
 var pin = null;
+var light = null;
 //var board = new five.Board();
-var board = new five.Board({
-  io: new Playground({
-    port: "com4",
+var board;
 
-    // Passing Firmata options through:
-    // Circuit Playground Firmata seems not to report version before timeout,
-    // lower timeout to reduce initial connection time.
-    reportVersionTimeout: 200
-  })
-});
+function setupBoard() {
+    board = new five.Board({
+        io: new Playground({
+            port: "com4",
 
-board.on("ready", function() {
-    var led = new five.Led(13);
-    var servo = new five.Servo(12);
-//    pin = new five.Pin({pin: 10, mode: five.Pin.INPUT});
-    pin = new five.Pin("A9");
-    //pin = new five.Pin("D9");
-    /*
-    pin = new five.Pin({pin: 10,
-                          type: "analog",
-                          mode: five.Pin.ANALOG});
-    */
-    led.blink(1000);
-    console.log("Pin:", pin);
+            // Passing Firmata options through:
+            // Circuit Playground Firmata seems not to report version before timeout,
+            // lower timeout to reduce initial connection time.
+            reportVersionTimeout: 200
+        })
+    });
+
+    board.on("ready", function() {
+        console.log("**** board ready ****");
+        var led = new five.Led(13);
+        var servo = new five.Servo(12);
+    //    pin = new five.Pin({pin: 10, mode: five.Pin.INPUT});
+        pin = new five.Pin("A9");
+        light = new five.Light("A5");
+        led.blink(1000);
+        console.log("Pin:", pin);
     
-    var accelerometer = new five.Accelerometer({
-        controller: Playground.Accelerometer
-    });
+        var accelerometer = new five.Accelerometer({
+         controller: Playground.Accelerometer
+        });
 
-    accelerometer.on("change", (data) => {
-        //console.log("acc data: "+JSON.stringify(data));
-        console.log("acc data: "+data.x+" "+data.y+" "+data.z);
-        console.log("sock: "+sock);
-        if (sock) {
-            console.log("sending acc.change "+data.x+" "+data.y+" "+data.z);
-            var acc = {x: data.x, y: data.y, z: data.z};
-            //sock.emit("acc.change", data.x+" "+data.y+" "+data.z);
-            sock.emit("acc.change", acc);
+        accelerometer.on("change", (data) => {
+            //console.log("acc data: "+JSON.stringify(data));
+            console.log("acc data: "+data.x+" "+data.y+" "+data.z);
+            console.log("sock: "+sock);
+            if (sock) {
+                console.log("sending acc.change "+data.x+" "+data.y+" "+data.z);
+                var acc = {x: data.x, y: data.y, z: data.z};
+                //sock.emit("acc.change", data.x+" "+data.y+" "+data.z);
+                sock.emit("acc.change", acc);
+            }
+            //piezo.frequency(data.double ? 1500 : 500, 50);
+        });
+
+        if (light) {
+            light.on("change", (data) => {
+                //console.log("light: "+JSON.stringify(data));
+                //console.log("lux:", data.lux);
+                if (sock) {
+                    sock.emit("light.change", data);
+                }
+            });
         }
-        //piezo.frequency(data.double ? 1500 : 500, 50);
-    });
-
-
-    pin.on("data", (data) => {
-        console.log("pin: "+data);
-    });
-
-    io.on("connection", function(socket) {
-        sock = socket;
-        console.log("Got connection...");
-        socket.on("change:interval", function(data) {
-            console.log("data: "+data);
-            led.blink(data);
+        if (pin) {
+            pin.on("data", (data) => {
+                //console.log("pin: "+data);
+                if (sock) {
+                    sock.emit("pin.change", data);
+                }
+            });
+        }
+        io.on("connection", function(socket) {
+            sock = socket;
+            console.log("Got connection...");
+            socket.on("change:interval", function(data) {
+                console.log("data: "+data);
+                led.blink(data);
+            });
+            socket.on("servo.set", function(data) {
+                //console.log("servo value: "+data);
+                servo.to(data);
+            });
         });
-        socket.on("servo.set", function(data) {
-            //console.log("servo value: "+data);
-            servo.to(data);
-        });
     });
-});
+}
 
 app.use(express.static(__dirname + "/.."));
 app.use(bodyParser.json());
@@ -91,4 +104,7 @@ app.get("/*", function (req, res) {
     res.sendFile(__dirname + req.path);
 });
 
+setupBoard();
+var argv = process.argv;
+console.log("argv:", argv);
 server.listen(3000);
