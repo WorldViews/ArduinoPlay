6917f80886463484ab103d9e9cf5f13d02aff4ba
jtan96 <jtman1226@gmail.com>
2018-07-26 14:56:18 -0700

    cleaned up code

--- Playground/js/ballTeleport.js
+++ Playground/js/ballTeleport.js
@@ -10,7 +10,7 @@ class Teleporter {
         this.t0 = getClockTime();
         this.closeGate();
     }
-    
+
     observeSensor(val){
         //console.log("sensor  val: "+ val)
         //console.log("runtime:"+dt);
@@ -39,43 +39,4 @@ class Teleporter {
         this.socket.emit("servo.set",180);
         this.tObserve=null;
     };
-}
-
-var socket=null;
-var t0;
-var tObserve=null;
-var t;
-function initTeleporter(sock){
-    socket=sock;
-    t0=getClockTime();
-    closeGate();
-};
-
-function observeSensor(val){
-    //console.log("sensor  val: "+ val)
-    //console.log("runtime:"+dt);
-    t=getClockTime();
-    if(val>30){
-        tObserve= getClockTime();
-    };
-    if (tObserve != null){
-        var dt = t-tObserve;
-        if (dt > 2){
-            console.log("foo");
-            openGate();
-        };
-    }
-    if (dt > 3){
-        closeGate();
-    };
-};
-
-function openGate(){
-    socket.emit("servo.set",0);
-    //tObserve=null;
-};
-
-function closeGate(){
-    socket.emit("servo.set",180);
-    tObserve=null;
 };
