256bd3ab0c4f8a8eca869cd2d3643846cbca40f1
donkimber <donkimber@gmail.com>
2018-08-13 14:55:37 -0700

    started moving some stuff into MUSEDefs.js

---
+++ Playground/js/MUSEDefs.js
@@ -0,0 +1,8 @@
+
+var MUSE = {};
+
+MUSE.getParameterByName = function(name) {
+    var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+    return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+}
+
