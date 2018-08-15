0f3997735cd4b1f6f0af64f1db1c67e1a40e58ff
jtan96 <jtman1226@gmail.com>
2018-08-14 18:39:58 -0700

    frist version of complete magic tunnel

--- Playground/js/TrainSystem.js
+++ Playground/js/TrainSystem.js
@@ -1,6 +1,11 @@
 
 var TRAIN_OBJS = {};
 
+function warning(str) {
+    console.log(str);
+    alert(str);
+}
+
 function getTrainObj(train)
 {
     if (TRAIN_OBJS[train] == null)
@@ -13,7 +18,7 @@ function handleState(msg)
     var train = msg.train;
     var tobj = getTrainObj(train);
     tobj.state = msg.state;
-        
+
     if (train == "train1")
         $("#train1State").html(msg.state);
     else if (train == "train2")
@@ -37,7 +42,7 @@ function handleProximity(msg)
 
 function handleMessage(msg)
 {
-    console.log("got ", msg);
+    //console.log("got ", msg);
     var train = msg.train;
     if (msg.msgType == "train.newState") {
         handleState(msg);
@@ -59,6 +64,8 @@ class TrainSystem
 {
     constructor(portal) {
         var inst = this;
+        this.movingTrain = null;//remove soon
+        this.programState = "S1";
         this.portal = portal;
         portal.sock.on("MUSE.IOT", msg => {
             handleMessage(msg);
@@ -82,7 +89,20 @@ class TrainSystem
     }
 
     updateProgram() {
-        console.log("updateProgram");
+        /* We will have a state machine with two main states.
+
+        S1: THis state means train1 is not yet in the tunnnel,
+        and train2 is in tunnel waiting to go.
+
+        S1b: This state is when we are in passthrough, waiting for
+        train2 to exit tunnel.;
+
+        S2: This state means train2 is clear of the tunnel,
+        and train1 is in tunnel waiting to go.
+
+        */
+        $("#programState").html(this.programState);
+        console.log("updateProgram state: "+this.programState);
         var tobj1 = getTrainObj("train1");
         var tobj2 = getTrainObj("train2");
         if (tobj1 == null || tobj2 == null) {
@@ -91,7 +111,35 @@ class TrainSystem
         }
         console.log("tobj1:", tobj1);
         console.log("tobj2:", tobj2);
+        if (tobj1.location == "TUNNEL" && tobj2.location == "TUNNEL") {
+            if (tobj1.state == "Stopped" && tobj2.state == "Stopped") {
+                console.log("******* PASSTHROUGH ********")
+                if (this.programState == "S1") {
+                    this.moveForward("train2");
+                    this.programState = "S1b";
+                }
+                else if (this.programState == "S2") {
+                    this.moveReverse("train1");
+                    this.programState = "S2b";
+                }
+            }
+            else {
+                console.log("*** TRANSITION OUT OF TUNNEL ***");
+            }
+        }
+        if (this.programState == "S1b" && tobj2.state == "Forward")
+            this.programState = "S2";
+        if (this.programState == "S2b" && tobj1.state == "Reverse")
+            this.programState = "S1";
+        /*
+        if (tobj1.state != "Stopped")
+            this.movingTrain = "train1";
+        else if (tobj2.state != "Stopped")
+            this.movingTrain = "train2";
+        else {
+            this.movingTrain = null;
+        }
+        */
+        $("#programState").html(this.programState);
     }
 }
-
-
